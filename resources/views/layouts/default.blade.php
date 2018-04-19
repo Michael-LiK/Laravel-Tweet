e0105f64c01b7a8be1d46064baaa2478379ffc51
Michael-LiK <you@example.com>
2018-04-19 18:06:58 +0800

    fix

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -8,9 +8,9 @@
         @include('layouts._header')
     <div class="container">
        <div class="col-md-offset-1 col-md-10">
+        @include('shared._messages')
         @yield('content')
         @include('layouts._footer')
-
        </div>
     </div>  
   </body>
