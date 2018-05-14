81e94155d5f1da050408978247b0b0f323f87f0e
vagrant <vagrant@homestead>
2018-05-14 06:43:39 +0000

    change Slogan

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -3,7 +3,7 @@
     <small class="slogon">
       <img class="brand-icon" src="https://lccdn.phphub.org/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34">
       <a href="https://laravel-china.org/courses">
-        刻意练习，每日精进
+        Study hard and making progress everyday.
       </a>
     </small>
     <nav>
